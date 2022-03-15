568ac5761d6d838a4f0dd4a670ad38a702220b8d
sebyyyy <94870277+EmaHermankova@users.noreply.github.com>
2022-03-15 17:53:56 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
 package com.example.heeelp;
 
+import javafx.animation.FadeTransition;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
@@ -11,6 +13,7 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -20,18 +23,23 @@ public class Main extends Application {
 
     private static final int P = 8;
     private static final int R = 4;
+    private K selected = null;
+    private int pocetKliknuti = 2;
 
-    private Parent Vytvor(){
+    private Parent vytvor(){
         Pane root = new Pane();
         root.setPrefSize(600,600);
         char c = 'A';
         List<K> tiles = new ArrayList<>();
+
         for(int i=0; i<P; i++){
             tiles.add(new K(String.valueOf(c)));
             tiles.add(new K(String.valueOf(c)));
             c++;
         }
+
         Collections.shuffle(tiles);
+
         for(int i=0; i<tiles.size(); i++){
             K tile = tiles.get(i);
             tile.setTranslateX(50*(i%R));
@@ -41,29 +49,72 @@ public class Main extends Application {
         return root;
     }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setScene(new Scene(vytvor()));
+        primaryStage.show();
+     }
+
     private class K extends StackPane {
+        private Text text = new Text();
 
         public K (String value){
             Rectangle ram = new Rectangle(50,50);
             ram.setFill(null);
             ram.setStroke(Color.BLACK);
-            Text text = new Text(value);
+            text.setText(value);
             text.setFont(Font.font(30));
             setAlignment(Pos.CENTER);
             getChildren().addAll(ram, text);
+            setOnMouseClicked(this::handleMouseClicked);
+            zavrit();
         }
-    }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        primaryStage.setScene(new Scene(Vytvor()));
-        primaryStage.show();
+        public void handleMouseClicked(MouseEvent event){
+            if(jeOtevreno() || pocetKliknuti == 0)
+                return;
+            pocetKliknuti--;
 
+            if(selected==null){
+                selected=this;
+                otevrit(() -> {});
+            }
 
+            else {
+                otevrit(() -> {
+                    if(!maStejnouHodnotu(selected)){
+                        selected.zavrit();
+                        this.zavrit();
+                    }
+                    selected = null;
+                    pocetKliknuti = 2;
+                });
+            }
+        }
+
+        public boolean jeOtevreno(){
+            return text.getOpacity()==1;
+        }
+
+        public void otevrit(Runnable action){
+            FadeTransition ft = new FadeTransition(Duration.seconds(0.5), text);
+            ft.setToValue(1);
+            ft.setOnFinished(e -> action.run());
+            ft.play();
+        }
+
+        public void zavrit(){
+            FadeTransition ft = new FadeTransition(Duration.seconds(0.5), text);
+            ft.setToValue(0);
+            ft.play();
+        }
+
+        public boolean maStejnouHodnotu(K other){
+            return text.getText().equals(other.text.getText());
+        }
     }
 
     public static void main(String[] args) {
         launch(args);
     }
-
 }
