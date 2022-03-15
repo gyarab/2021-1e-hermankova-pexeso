5453a7740888dd9c8f00b3771484b4dfedc6dd10
sebyyyy <94870277+EmaHermankova@users.noreply.github.com>
2022-03-15 16:35:50 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -12,19 +12,38 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main extends Application {
+
+    private static final int P = 8;
+    private static final int R = 4;
 
     private Parent Vytvor(){
         Pane root = new Pane();
         root.setPrefSize(600,600);
-        Tile tile = new Tile("A");
-        root.getChildren().add(tile);
+        char c = 'A';
+        List<K> tiles = new ArrayList<>();
+        for(int i=0; i<P; i++){
+            tiles.add(new K(String.valueOf(c)));
+            tiles.add(new K(String.valueOf(c)));
+            c++;
+        }
+        Collections.shuffle(tiles);
+        for(int i=0; i<tiles.size(); i++){
+            K tile = tiles.get(i);
+            tile.setTranslateX(50*(i%R));
+            tile.setTranslateY(50*(i/R));
+            root.getChildren().add(tile);
+        }
         return root;
     }
 
-    private class Tile extends StackPane {
+    private class K extends StackPane {
 
-        public Tile (String value){
+        public K (String value){
             Rectangle ram = new Rectangle(50,50);
             ram.setFill(null);
             ram.setStroke(Color.BLACK);
